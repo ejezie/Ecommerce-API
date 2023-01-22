cca3e44f816d75a3011da3d84b17bb22cefc6b3d
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-01-22 06:46:35 +0100

    error hanldler

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
 import express from "express";
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 dotenv.config();
 import { dbConnect } from "./configs/dbConnect.js";
 import authRoute from "./routes/authRoutes.js";
 import bodyParser from "body-parser";
+import { errorHandler, notFound } from "./middlewares/errorHandler.js";
 
 dbConnect();
 
@@ -11,11 +12,15 @@ const PORT = process.env.PORT || 5000;
 
 const app = express();
 
-app.use(bodyParser())
-app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser());
+app.use(bodyParser.urlencoded({ extended: false }));
+// app.use(cors());
 
-app.use('/api/user', authRoute)
+app.use("/api/user", authRoute);
+
+app.use(notFound)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`App is listening on port ${PORT}`)
-})
+  console.log(`App is listening on port ${PORT}`);
+});
--- middlewares/errorHandler.js
+++ middlewares/errorHandler.js
@@ -4,3 +4,12 @@ export const notFound = (req, res, next) => {
   res.status(404);
   next(error);
 };
+
+export const errorHandler = (err, req, res, next) => {
+    const statuscode = res.statusCode== 200 ? 500 : res.statusCode;
+    req.status(statuscode);
+    res.json({
+        message: err?.message,
+        stack: err?.stack
+    })
+}
