253fd7335cd1e1656d4087b9e27c5131d12c4072
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-01-23 07:45:49 +0100

    hash pasword pre save

--- models/userModel.js
+++ models/userModel.js
@@ -1,32 +1,38 @@
-import mongoose from 'mongoose'; 
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
-const {Schema, model} = mongoose
+const { Schema, model } = mongoose;
 
 // Declare the Schema of the Mongo model
 const userSchema = new Schema({
-    firstname:{
-        type:String,
-        required:true,
-    },
-    lastname:{
-        type:String,
-        required:true,
-    }, 
-    email:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    mobile:{
-        type:String,
-        required:true,
-        unique:true,
-    },
-    password:{
-        type:String,
-        required:true,
-    },
+  firstname: {
+    type: String,
+    required: true,
+  },
+  lastname: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  mobile: {
+    type: String,
+    required: true,
+    unique: true,
+  },
+  password: {
+    type: String,
+    required: true,
+  },
 });
 
+userSchema.pre("save", async function(next) {
+    const salt = await bcrypt.genSaltSync(10)
+    this.password = bcrypt.hash(this.password, salt)
+})
+
 //Export the model
-export default model('User', userSchema);
+export default model("User", userSchema);
