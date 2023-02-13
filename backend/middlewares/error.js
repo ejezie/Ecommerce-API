b08aa55a9b780e7c04ac5594bc9c62ad27cedd70
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-02-13 19:30:12 +0100

    mongoose id error/validation error

--- backend/middlewares/error.js
+++ backend/middlewares/error.js
@@ -32,6 +32,18 @@ export default (err, req, res, next) => {
     let error = {...err};
     error.message = err.message;
 
+    // Handling wrong mongoose Id Error
+    if (err.name === "CastError") {
+      const message = `Resource not found. Invalid:  ${err.path}`;
+      error = new ErrorHandler(message, 400);
+    }
+
+    // Handling mongooose validation error
+    if(err.name === "ValidationError") {
+      const message = Object.values(err.errors).map(value => value.message);
+      error = new ErrorHandler(message, 400);
+    }
+
     res.status(err.statusCode).json({
       success: false,
       message: error.message,
