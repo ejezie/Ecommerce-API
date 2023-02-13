73e3a7b963dfb24b9e15748b21d7c796f3137dd6
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-02-13 06:54:12 +0100

    fix asyncError Handler

--- backend/middlewares/catchAsyncErrors.js
+++ backend/middlewares/catchAsyncErrors.js
@@ -1,4 +1,4 @@
-const func = (req, res, next) =>
+const catchAsyncErrors = func => (req, res, next) =>
   Promise.resolve(func(req, res, next)).catch(next);
 
-export default func;
+export default catchAsyncErrors;
