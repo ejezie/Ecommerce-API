76ee0632268ea277a05d0405d53ba7487df93a46
EJEZIE CHINEDU EMMANUEL <shady07chinex@gmail.com>
2023-02-27 00:07:19 +0100

    registre user

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,19 +1,31 @@
 import User from "../models/userModel.js";
 import asyncHandler from "express-async-handler";
+import ErrorHandler from "../utils/errorHandler.js";
+import catchAsyncErrors from "../middlewares/catchAsyncErrors.js";
 
-export const createUser = asyncHandler(async (req, res) => {
-  const email = req.body.email;
+// Register user => api/v1/register
+export const createUser = catchAsyncErrors(async (req, res) => {
+  const {firstname, lastname, email, password} = req.body;
   const userExist = await User.findOne({ email: email });
 
   if (!userExist) {
-    const newUser = await User.create(req.body);
+    const newUser = await User.create({
+      firstname, 
+      lastname, 
+      password,
+      email,
+      avater: {
+        public_id: "",
+        url: "",
+      }
+    });
     res.json(newUser);
   } else {
     throw new Error("User already exist");
   }
 });
 
-export const loginUser = asyncHandler(async (req, res) => {
+export const loginUser = catchAsyncErrors(async (req, res) => {
   const { email, password } = req.body;
   // Find user, check user exists
   const findUser = await User.findOne({ email });
